ec4692f720b41aaabca6b27176e56fa18f97daf2
Amir Ravan <amir.h.rnn@gmail.com>
2024-03-19 05:04:32 +0330

    First commit

---
+++ HW2/ProductManagementSystem/src/main/java/edu/webclass/restapi/Product/Management/System/controllers/ProductsController.java
@@ -0,0 +1,32 @@
+package edu.webclass.restapi.Product.Management.System.controllers;
+
+import edu.webclass.restapi.Product.Management.System.models.Product;
+import edu.webclass.restapi.Product.Management.System.models.dto.ProductDto;
+import edu.webclass.restapi.Product.Management.System.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/list")
+    public List<ProductDto> listAllProducts(){
+        return productService.findAllProducts().stream().map(product -> new ProductDto(product)).toList();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable String id) {
+        return productService.findProductById(id);
+    }
+
+    @PostMapping("/add")
+    public boolean addProduct(@RequestHeader("name") String title,@RequestHeader String brand,@RequestHeader int price){
+        return productService.addProduct(title,brand,price);
+    }
+}
---
+++ HW2/ProductManagementSystem/src/main/java/edu/webclass/restapi/Product/Management/System/services/ProductService.java
@@ -0,0 +1,36 @@
+package edu.webclass.restapi.Product.Management.System.services;
+
+import edu.webclass.restapi.Product.Management.System.models.Product;
+import edu.webclass.restapi.Product.Management.System.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAllProducts(){
+        return productRepository.findAllProducts();
+    }
+
+    public boolean addProduct(String title, String brand, int price){
+        return productRepository.createNewProduct(new Product(title,brand,price));
+    }
+
+    public Product findProductById(String id) {
+        Product target = productRepository.findProductById(id);
+        if (target == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "product not found"
+            );
+        }
+        return target;
+    }
+
+}
